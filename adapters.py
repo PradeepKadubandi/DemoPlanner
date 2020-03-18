e89e69065ba84dde05b609a0b924ac006fea1174
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2020-03-18 11:34:05 -0700

    Code changes to follow what Peter did : At eval frequency, run the complete train and test data through network to get the loss.
    (However since training data is too huge for V2, I capped 10000 rows for
    training loss)
    
    And also support to show reconstruction images while training is in progress.

--- adapters.py
+++ adapters.py
@@ -10,3 +10,9 @@ def demopl_v1_data_to_img(data, batch_size):
     x_dim = 2
     img_res = 32
     return 255 * data.view(batch_size, img_res, img_res)
+
+def demopl_v2_data_adapter(data):
+    return demopl_v1_data_adapter(data)
+
+def demopl_v2_data_to_img(data, batch_size):
+    return demopl_v1_data_to_img(data, batch_size)
--- runner.py
+++ runner.py
@@ -13,15 +13,21 @@ from io import StringIO
 from utils import writeline
 
 class ExptRunner:
-    def __init__(self, expt_prefix, net, data_adapter_func, loss_adapter_func):
+    def __init__(self, expt_prefix, net, train_data, test_data,
+                    data_adapter_func, loss_adapter_func, data_to_img_func=None):
         self.expt_prefix = expt_prefix
         self.net = net
+        self.train_data = train_data
+        self.test_data = test_data
         self.data_adapter_func = data_adapter_func
         self.loss_adapter_func = loss_adapter_func
+        self.data_to_img_func = data_to_img_func
         self.expt_name = time.strftime('%m-%d-%H-%M-%S-') + expt_prefix
         self.log_folder = 'runs/' + self.expt_name
         self.writer = SummaryWriter(self.log_folder)
         self.prev_offset = 0
+        self.train_loader = DataLoader(train_data, batch_size=50, shuffle=True)
+        self.test_loader = DataLoader(test_data, batch_size=3, shuffle=True)
 
     def log_network_details(self):
         header1 = StringIO()
@@ -56,7 +62,7 @@ class ExptRunner:
         header1.close()
 
 
-    def train(self, epochs, training_loader, resume_previous_training=False):
+    def train(self, epochs, resume_previous_training=False, shouldShowReconstruction=False):
         '''
         For now, resume_previous_training can be set to True immediately after one training run
         and should use the same Trainer object with more epochs, could consider better ways in future.
@@ -71,8 +77,9 @@ class ExptRunner:
 
         builder = StringIO()
         running_loss = 0.0
+        eval_freq = 1000
         for epoch in range(epochs):
-            for i, data in enumerate(training_loader, 0):
+            for i, data in enumerate(self.train_loader, 0):
                 data = data.float()
                 optimizer.zero_grad()
 
@@ -83,12 +90,32 @@ class ExptRunner:
                 optimizer.step()
                 
                 running_loss += loss.item()
-                if i % 100 == 99:
-                    avg_loss = running_loss / 100
-                    writeline(builder, '[%d, %5d] loss: %.3f' % (self.prev_offset+epoch+1, i+1, avg_loss))
-                    index = (self.prev_offset+epoch) * len(training_loader) + i
+                if i % eval_freq == (eval_freq-1):
+                    avg_loss = running_loss / eval_freq
+                    writeline(builder, '[%d, %5d] Average Minibatch loss: %.3f' % (self.prev_offset+epoch+1, i+1, avg_loss))
+                    index = (self.prev_offset+epoch) * len(self.train_loader) + i
                     self.writer.add_scalar('training_loss', avg_loss, index)
                     running_loss = 0.0
+
+                    with torch.no_grad():
+                        train_input = self.data_adapter_func(self.train_data.data[:10000].float())
+                        train_out, train_loss = self.loss_adapter_func(self.net, train_input)
+                        writeline(builder, 'MinibatchIndex {}: Training Loss (Max 10000 rows): {}'.format(index, train_loss))
+
+                        test_input = self.data_adapter_func(self.test_data.data.float())
+                        test_out, test_loss = self.loss_adapter_func(self.net, test_input)
+                        writeline(builder, 'MinibatchIndex {}: Test Loss: {}'.format(index, test_loss))
+
+                    if self.data_to_img_func is not None:
+                        n = 1
+                        filename='reconstruction_at_index_{}'.format(index)
+                        printHeader="Reconstruction At Index {}".format(index) if shouldShowReconstruction else None
+                        self.save_matplotlib_comparison(n, 
+                            self.data_to_img_func(test_input[:n], n),
+                            self.data_to_img_func(test_out[:n], n),
+                            filename=filename,
+                            printHeader=printHeader,
+                            shouldShow=shouldShowReconstruction)
 
         self.prev_offset += epochs
         writeline(builder, 'Total time taken for training {} sec.'.format(time.time() - start))
@@ -99,7 +126,10 @@ class ExptRunner:
 
         torch.save(self.net.state_dict(), self.log_folder + '/autoenc.pth')
 
-    def save_matplotlib_comparison(self, rows, ip_imgs, op_imgs, cmapstr='gray'):
+    def save_matplotlib_comparison(self, rows, ip_imgs, op_imgs, filename,
+                                    cmapstr='gray', shouldShow=True, printHeader=None):
+        if printHeader is not None:
+            print (printHeader)
         fig = plt.figure()
         for r in range(rows):
             ax = plt.subplot(rows, 2, r*2 + 1)
@@ -116,27 +146,33 @@ class ExptRunner:
             if r==0:
                 ax.title.set_text('Recontructed')
 
-        plt.show()
-        fig.savefig(self.log_folder + '/reconstruction.png')
-        self.writer.add_figure('Reconstruction Sample', fig)
+        if shouldShow:
+            plt.show()
+        fig.savefig(self.log_folder + '/' + filename + '.png')
+        self.writer.add_figure(filename, fig)
 
 
-    def test(self, test_data_loader, data_to_img_func=None):
+    def test(self):
         self.net.eval()
         builder = StringIO()
         test_loss = 0.0
         with torch.no_grad():
-            for i, data in enumerate(test_data_loader, 0):
+            for i, data in enumerate(self.test_loader, 0):
                 data = data.float()
                 ip_batch = self.data_adapter_func(data)
                 op_batch, loss = self.loss_adapter_func(self.net, ip_batch)
 
                 test_loss += loss.item()
-                if data_to_img_func is not None and i == 0:
-                    n = min(data.size(0), 8)
-                    self.save_matplotlib_comparison(n, data_to_img_func(ip_batch[:n], n), data_to_img_func(op_batch[:n], n))
+                if self.data_to_img_func is not None and i == 0:
+                    # n = min(data.size(0), 8)
+                    n = 1
+                    self.save_matplotlib_comparison(n, 
+                        self.data_to_img_func(ip_batch[:n], n),
+                        self.data_to_img_func(op_batch[:n], n),
+                        filename='final_reconstruction',
+                        printHeader="Final Reconstruction")
 
-        avg_loss = test_loss / len(test_data_loader.dataset)
+        avg_loss = test_loss / len(self.test_loader.dataset)
         writeline(builder, "Average Test Loss: {:.3f}".format(avg_loss))
         with open(self.log_folder + '/test_log.txt', 'w') as f:
             f.write(builder.getvalue())
