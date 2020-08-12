b5edc42f062d835cada7135207d5251fc9ffeb4f
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2020-08-12 15:15:36 -0700

    Minor fixes and changes to evaluation code

--- env/simulator.py
+++ env/simulator.py
@@ -10,6 +10,8 @@ import os
 import cv2
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+from io import StringIO
+from utils import writeline
 
 class simulator:
     def __init__(self):
@@ -22,6 +24,7 @@ class simulator:
     def reset(self, initialState):
         self.env.reset()
         initial_pos = self.env.sim.data.qpos
+        initialState = initialState.cpu()
         initial_pos[:x_dim+y_dim] = initialState[:x_dim+y_dim].clone()
         self.env.set_state(initial_pos, self.env.sim.data.qvel)
         self.env.visualize_goal_indicator(initialState[x_dim:x_dim+y_dim].clone())
@@ -42,11 +45,11 @@ class simulator:
         return torch.as_tensor(cv2.cvtColor(cropped_rgb, cv2.COLOR_RGB2GRAY)).float()
 
 class evaluator:
-    def __init__(self, chkpt_file, data, device, policy_takes_image, persist_to_disk):
+    def __init__(self, chkpt_file, device, policy_takes_image, persist_to_disk):
         self.log_folder = os.path.dirname(chkpt_file)
+        self.device = device
         self.chkpt = torch.load(self.log_folder + '/train_checkpoint.tar', map_location=device)
         self.net = self.chkpt['model']
-        self.data = data
         self.policy_takes_image = policy_takes_image
         self.persist_to_disk = persist_to_disk
         self.sim = simulator()
@@ -55,6 +58,7 @@ class evaluator:
         gt_states, gt_images = gt_trajectory[states_key], gt_trajectory[images_key]
         self.sim.reset(gt_states[0])
         curr_state, curr_image = self.sim.getState()
+        curr_state, curr_image = curr_state.to(self.device), curr_image.to(self.device)
         states = torch.as_tensor(curr_state.reshape(1, -1))
         images = torch.as_tensor(curr_image.reshape(1, -1))
         actions = None
@@ -62,23 +66,24 @@ class evaluator:
             with torch.no_grad():
                 action = self.net(curr_image if self.policy_takes_image else curr_state)
                 if actions is None:
-                    actions = torch.as_tensor(action.reshape(1, -1))
+                    actions = torch.as_tensor(action.reshape(1, -1)).to(self.device)
                 else:
                     actions = torch.cat((actions, action.reshape(1, -1)), dim=0)
-            curr_state, curr_image = self.sim.step(action.numpy())
+            curr_state, curr_image = self.sim.step(action.cpu().numpy())
+            curr_state, curr_image = curr_state.to(self.device), curr_image.to(self.device)
             states = torch.cat((states, curr_state.reshape(1, -1)), dim=0)
             images = torch.cat((images, curr_image.reshape(1, -1)), dim=0)
         actions = torch.cat((actions, torch.zeros_like(actions[0]).reshape(1, -1)), dim=0)
         states = torch.cat((states, actions), dim=1)
         return {states_key: states, images_key: images}
 
-    def evaluate(self):
-        N = len(self.data)
+    def evaluate(self, data, target_folder):
+        N = len(data)
         errors = torch.zeros((N, 6))
         allLabels = []
         allPredictions = []
         for index in range(N):
-            labels = self.data[index]
+            labels = data[index]
             predictions = self.rollout(labels)
             allLabels.append(labels)
             allPredictions.append(predictions)
@@ -98,18 +103,20 @@ class evaluator:
         aggregates[3], aggregates[2] = torch.min(errors, dim=0)
         aggregates[5], aggregates[4] = torch.max(errors, dim=0)
         lower_threshholds = torch.Tensor([-1, 10]) # Total trajectories, trajectories of len > 10
-        upper_threshholds = torch.Tensor([0.01, 0.01, 0.01, 0.01]) # trajectories of policy loss, state loss, dynamics step loss, goal loss < 0.01
+        upper_threshholds = torch.Tensor([0.1, 0.1, 0.1, 0.1]) # trajectories of policy loss, state loss, dynamics step loss, goal loss < 0.01
         aggregates[6] = torch.sum(torch.cat((
             torch.gt(errors[:, :2], lower_threshholds),
             torch.lt(errors[:, 2:], upper_threshholds)), dim=1), dim=0)
         agg = aggregates.numpy()
         
-        result_folder = self.log_folder + '/Evaluations'
+        result_folder = os.path.join(self.log_folder, target_folder)
         if self.persist_to_disk:
             if not os.path.exists(result_folder):
                 os.makedirs(result_folder)
 
-            with open(result_folder + '/trajectory_eval_metrics.csv', 'a') as f:
+            with open(result_folder + '/trajectory_eval_metrics.csv', 'w') as f:
+                np.savetxt(f, errors.numpy(), fmt='%5.2f', delimiter=',')
+            with open(result_folder + '/trajectory_eval_summary.csv', 'w') as f:
                 f.write('Trajectory Index,Trajectory Length,Policy Accuracy,Policy L1 Loss,Dynamics Loss (step),Goal Deviation\n')
                 f.write('Aggregates:\n')
                 for i in range(len(aggregate_row_headers)):
@@ -117,9 +124,8 @@ class evaluator:
                     f.write(',')
                     f.write(str.join(',', [str(val) for val in agg[i, 1:]]))
                     f.write('\n')
-                f.write('\nInidividual:\n')
-                np.savetxt(f, errors.numpy(), fmt='%5.2f', delimiter=',')
-        
+
+        builder = StringIO()
         sample_descriptions = ['Least Policy L1 Error',
                             'Least Trajectory Error','Least Goal Error',
                             'Highest Policy L1 Error',
@@ -127,19 +133,22 @@ class evaluator:
         # For policy, min means worst, for other errors, min means best
         indices = torch.cat((aggregates[2, 2:5], aggregates[4, 2:5])).long()
         values = torch.cat((aggregates[3, 2:5], aggregates[5, 2:5]))
-        print ('-------------------------------------------------------------')
-        print (self.log_folder)
-        print ('-------------------------------------------------------------')
-        print ('Number of Trajectories Ended up Within Goal Region: {}'.format(aggregates[6, 5].item()))
-        print ('Average Policy L1 Loss: {}'.format(aggregates[1, 2].item()))
-        print ('Average State Loss: {}'.format(aggregates[1, 3].item()))
-        print ('Average Trajectory Loss: {}'.format(aggregates[1, 4].item()))
-        print ('Average Goal Loss: {}'.format(aggregates[1, 5].item()))
-        print ('')
+        writeline(builder, '-------------------------------------------------------------', out_to_console=True)
+        writeline(builder, self.log_folder, out_to_console=True)
+        writeline(builder, '-------------------------------------------------------------')
+        writeline(builder, 'Number of Trajectories Ended up Within Goal Region ({}): {}'.format(upper_threshholds[-1].item(), aggregates[6, 5].item()), out_to_console=True)
+        writeline(builder, 'Average Policy L1 Loss: {}'.format(aggregates[1, 2].item()), out_to_console=True)
+        writeline(builder, 'Average Trajectory Loss: {}'.format(aggregates[1, 3].item()), out_to_console=True)
+        writeline(builder, 'Average Combined State Loss: {}'.format(aggregates[1, 4].item()), out_to_console=True)
+        writeline(builder, 'Average Combined Goal Loss: {}'.format(aggregates[1, 5].item()), out_to_console=True)
+        writeline(builder, '', out_to_console=True)
         for i in range(len(sample_descriptions)):
-            print ('{}: Index = {}, Value = {}'.format(sample_descriptions[i], indices[i], values[i]))
+            writeline(builder, '{}: Index = {}, Value = {}'.format(sample_descriptions[i], indices[i], values[i]), out_to_console=True)
             if i == 2:
-                print ('')
+                writeline(builder, '', out_to_console=True)
+
+        with open(result_folder + '/high_level_summary.txt', 'w') as f:
+            f.write(builder.getvalue())
 
         if self.persist_to_disk:
             for i in range(len(sample_descriptions)):
@@ -151,8 +160,8 @@ class evaluator:
                     for i in range(len(labels)):
                         fig = plt.figure()
                         plt.subplot(1,2,1)
-                        plt.imshow(labels[i, :].reshape(img_res,img_res), cmap=plt.get_cmap("gray"))
+                        plt.imshow(labels[i, :].reshape(img_res,img_res).cpu(), cmap=plt.get_cmap("gray"))
                         plt.subplot(1,2,2)
-                        plt.imshow(predictions[i, :].reshape(img_res,img_res), cmap=plt.get_cmap("gray"))
+                        plt.imshow(predictions[i, :].reshape(img_res,img_res).cpu(), cmap=plt.get_cmap("gray"))
                         pdf.savefig(fig)
                         plt.close(fig)
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 from io import StringIO
 import glob
 
-def writeline(builder, line, debug=False):
+def writeline(builder, line, out_to_console=False):
     builder.write(line)
     builder.write('\n')
-    if debug:
+    if out_to_console:
         print (line)
 
 def enumerate_files(rootdir='runs', extension='tar'):
