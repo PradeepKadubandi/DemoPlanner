8b1fd38c093cece03d5e294e1979c0614495607d
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2021-02-13 19:16:57 -0800

    Logger abstraction - Work In Progress

---
+++ logger.py
@@ -0,0 +1,12 @@
+from torch.utils.tensorboard import SummaryWriter
+
+class Logger:
+    def __init__(self, log_dir):
+        self.log_dir = log_dir
+        self.writer = SummaryWriter(log_dir)
+
+    def log_scalar(self, name, value, step):
+        self.writer.add_scalar(name, value, step)
+
+    def log_image(self, name, image, step):
+        self.writer.add_image(name, image, step)
